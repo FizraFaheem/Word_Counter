3822fdff03610437d75d3667d6d0807867ecfdd6
Fizra Faheem <160629677+FizraFaheem@users.noreply.github.com>
2024-07-01 07:08:01 -0700

    Word Counter
    "Harness the Power of Words: Your Ultimate Word Counter Tool!"

---
+++ main.ts
@@ -0,0 +1,35 @@
+// Importing inquirer and chalk packages
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+// Display a colourfull welcome message
+console.log(chalk.bold.cyanBright("\n \t\t Qirat Faheem - word counter"));
+console.log("=".repeat(60));
+
+// Prompt the user to enter a sentence
+let answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter a Sentence",
+    }
+]);
+// Trimming the sentence and splitting it into words based on "spaces"
+let words = answers.sentence.trim().split(" ");
+
+// Analysis of user input sentence
+console.log("=".repeat(60));
+console.log(chalk.bold("- Sentence Words:"));
+console.log(words);
+console.log(chalk.bold(`\n - Word Count: ${words.length}`));
+console.log("=".repeat(60));
+
+
+
+
+
+
+
+
+
+
